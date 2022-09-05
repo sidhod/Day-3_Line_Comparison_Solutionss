4b8aee45ff9dcadd17f3c857da34171cedf52980
unknown <sidhodhank2018@gmail.com>
2022-09-05 21:55:47 +0530

    Calculate length of line using Cartesion formula

--- Line_Comparison.java
+++ Line_Comparison.java
@@ -1,8 +1,19 @@
 /* Welcome Line Comparison Problems*/
+/*Calculate lenght of line AB*/
 package line_comparison_problem;
 
 public class Line_Comparison {
-	
+	public static void main(String[] args) {
+		int x1 = (int) Math.floor(Math.random()*10);
+		int x2 = (int) Math.floor(Math.random()*10);
+		int y1 = (int) Math.floor(Math.random()*10);
+		int y2 = (int) Math.floor(Math.random()*10);
+    	System.out.println("point A is"+" "+"("+x1 +","+y1 +")");
+    	System.out.println("point B is"+" "+"("+x2 +","+y2 +")");
+    	double len =Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+    	System.out.println("lenght of line AB "+ len);
+    }
 
 
 }
+
